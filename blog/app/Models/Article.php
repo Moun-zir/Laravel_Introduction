29595407d7a143f22188c3ad1076c4377450c52c
Moun-zir <ossenimounzir@gmail.com>
2024-11-27 12:29:43 +0100

    mise en place des eloquent

--- blog/app/Models/Article.php
+++ blog/app/Models/Article.php
@@ -17,4 +17,15 @@ class Article extends Model
       */
     // Ajout
     protected $fillable = ['title', 'body', 'user_id', 'image'];
+
+    // Un article n'a qu'un auteur
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    // Un article peut avoir plusieurs commentaires
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
